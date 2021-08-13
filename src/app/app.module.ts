2403ba63c0c4ca2bad6ddcc0ae8c57c3147d1a16
Jean Michel Fernandes Carvalho <50302236+Gl1tch42@users.noreply.github.com>
2021-08-13 00:10:30 -0300

    formGroup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +21,9 @@ import { FloatComponent } from './core/float/float.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+  }
 
   ngOnInit() {
+    this.initForm();
+  }
+
+  initForm() {
+    this.form = this.formBuilder.group({
+      fcEmail: ['', [Validators.required]],
+      fcPassword: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  login(): void {
+    console.log(this.form.value);
   }
 
 }
